3922a8d77de687208ed1a1dcb193b95b53b2f76b
MarcSchlichting <mschl@stanford.edu>
2021-07-01 10:45:33 -0700

    completed setup file for docker

--- setup_docker.jl
+++ setup_docker.jl
@@ -1,3 +1,6 @@
 using Pkg
 
 Pkg.develop(PackageSpec(url="./ObservationModels"))
+Pkg.develop(PackageSpec(url="./RiskSimulator.jl"))
+Pkg.develop(PackageSpec(url="./IntelligentDriving.jl"))
+Pkg.develop(PackageSpec(url="./STLCG.jl"))
