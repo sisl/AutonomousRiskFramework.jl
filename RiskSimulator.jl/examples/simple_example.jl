c741f653c8f4eb880d691a882fbafe0ae8e08c6e
Robert Moss <rmoss92@gmail.com>
2021-10-12 16:31:45 -0700

    Added simple_example.jl

---
+++ RiskSimulator.jl/examples/simple_example.jl
@@ -0,0 +1,9 @@
+using RiskSimulator
+system = IntelligentDriverModel()
+scenario = get_scenario(MERGING)
+planner = setup_ast(sut=system, scenario=scenario)
+search!(planner)
+fail_metrics = failure_metrics(planner)
+α = 0.2 # risk tolerance
+risk_metrics = risk_assessment(planner, α)
+risk = overall_area(planner, weights=𝐰, α=α)
