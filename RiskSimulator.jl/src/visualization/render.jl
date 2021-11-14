c11e0a7cd53ef861eac301d7692131a388f08623
kykim <kykim144@gmail.com>
2021-11-13 16:52:11 -0800

    Add another render example.

--- RiskSimulator.jl/src/visualization/render.jl
+++ RiskSimulator.jl/src/visualization/render.jl
@@ -8,7 +8,7 @@ using RiskSimulator
 
 # Creates a GIF of a simulation run with the given scenario type and initial conditions.
 # Examples:
-#   render_failure("animation.gif", MERGING, 3.20, 12.50, 36.72, 1.83)
+#   render_failure("animation.gif", MERGING, 20.48, 26.91, 34.72, 14.46)
 #   render_failure("animation.gif", STOPPING, 3.20, 12.50, 36.72, 1.83)
 function render_gif(filename::String, scenario_type::SCENARIO, s_sut::Float64, s_adv::Float64, v_sut::Float64, v_adv::Float64)
     scenario = get_scenario(scenario_type, s_sut=s_sut, s_adv=s_adv, v_sut=v_sut, v_adv=v_adv)
