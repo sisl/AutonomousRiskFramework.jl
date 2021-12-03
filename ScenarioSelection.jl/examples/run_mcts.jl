ec4bc5ff9ab61d24021db4284423ece299918059
Shubh Gupta <shubhg1996@gmail.com>
2021-12-02 18:07:45 -0800

    visualization code

--- ScenarioSelection.jl/examples/run_mcts.jl
+++ ScenarioSelection.jl/examples/run_mcts.jl
@@ -8,12 +8,12 @@ using ProgressMeter
 using D3Trees
 using MCTS
 
-mdp = ScenarioSearch(1, [])
+mdp = ScenarioSearch(1, [], [])
 
 planner = mcts_isdpw(mdp)
 
-a, info = action_info(planner, DecisionState(), tree_in_info=true; random=true)
+a, info = action_info(planner, DecisionState(), tree_in_info=true)
 t = D3Tree(info[:tree], init_expand=1);
 inchrome(t)
 
-save(raw"data\\mcts_random_risks_1000_ALL.jld2", Dict("risks:" => planner.mdp.cvars, "states:" => [], "is_prob:" => planner.mdp.isprob))
+save(raw"data\\mcts_random_IS_risks_1000_ALL.jld2", Dict("risks:" => planner.mdp.cvars, "states:" => [], "is_prob:" => planner.mdp.isprob))
