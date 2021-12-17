eaacb4898873da4c99df7d93ee4fa298374597f0
Robert Moss <rmoss92@gmail.com>
2021-12-16 17:32:13 -0800

    Solve using TD3

--- CARLAIntegration/adversarial_carla_env/test/solve_gda.jl
+++ CARLAIntegration/adversarial_carla_env/test/solve_gda.jl
@@ -30,26 +30,89 @@ function sac_solver(mdp)
     amax = [Float32(sensors[1][k]["upper"]) for k in ["lat", "lon", "alt"]]
     rand_policy = FunctionPolicy((s) -> Float32.(rand.(Uniform.(amin, amax))))
 
-    state_dim = length(rand(initialstate(mdp)))
+    state_dim = first(S.dims)
     action_dim = length(amin)
-    QSA() = ContinuousNetwork(Chain(Dense(state_dim+action_dim, 64, relu), Dense(64, 64, relu), Dense(64, action_dim)))
+    QSA() = ContinuousNetwork(Chain(Dense(state_dim+action_dim, 64, relu), Dense(64, 64, relu), Dense(64, 1)))
 
     function SAC_A()
         base = Chain(Dense(state_dim, 64, relu), Dense(64, 64, relu))
         mu = ContinuousNetwork(Chain(base..., Dense(64, action_dim)))
         logΣ = ContinuousNetwork(Chain(base..., Dense(64, action_dim)))
-        SquashedGaussianPolicy(mu, logΣ)
+        SquashedGaussianPolicy(mu, logΣ, 0.0000001)
     end
+
+    on_policy = ActorCritic(SAC_A(), DoubleNetwork(QSA(), QSA()))
 
     off_policy = (S=S,
                   ΔN=50,
-                  N=2, # NOTE: was 30_000 (then 100)
+                  N=100, # NOTE: was 30_000 (then 100)
                   buffer_size=Int(5e5),
-                  buffer_init=1000,
+                  buffer_init=1,
                   c_opt=(batch_size=100, optimizer=ADAM(1e-3)),
                   a_opt=(batch_size=100, optimizer=ADAM(1e-3)),
-                  π_explore=FirstExplorePolicy(5, rand_policy, GaussianNoiseExplorationPolicy(0.0000001f0, a_min=amin, a_max=amax))) # NOTE: was 1000
-    return SAC(; π=ActorCritic(SAC_A(), DoubleNetwork(QSA(), QSA())), off_policy...)
+                  log=(fns=[],),
+                  π_explore=on_policy)
+
+    return SAC(; π=on_policy, off_policy...)
+end
+
+
+function td3_solver(mdp)
+    global sensors
+
+    S = state_space(mdp)
+    amin = [Float32(sensors[1][k]["lower"]) for k in ["lat", "lon", "alt"]]
+    amax = [Float32(sensors[1][k]["upper"]) for k in ["lat", "lon", "alt"]]
+    rand_policy = FunctionPolicy((s) -> Float32.(rand.(Uniform.(amin, amax))))
+
+    state_dim = first(S.dims)
+    action_dim = length(amin)
+    QSA() = ContinuousNetwork(Chain(Dense(state_dim+action_dim, 64, relu), Dense(64, 64, relu), Dense(64, 1)))
+
+    TD3_A() = ContinuousNetwork(Chain(Dense(state_dim, 64, relu), Dense(64, 64, relu), Dense(64, action_dim), x -> x .* 0.0000001), action_dim)
+    
+    on_policy = ActorCritic(TD3_A(), DoubleNetwork(QSA(), QSA()))
+
+    off_policy = (S=S,
+                  ΔN=300,
+                  N=3000, # NOTE: was 30_000 (then 100)
+                  buffer_size=Int(5e5),
+                  buffer_init=1,
+                  c_opt=(batch_size=100, optimizer=ADAM(1e-3)),
+                  a_opt=(batch_size=100, optimizer=ADAM(1e-3)),
+                  log=(fns=[],),
+                  π_explore=GaussianNoiseExplorationPolicy(0.00000001f0))
+
+    return TD3(; π=on_policy, off_policy...)
+end
+
+
+function ppo_solver(mdp)
+    global sensors
+
+    S = state_space(mdp)
+    amin = [Float32(sensors[1][k]["lower"]) for k in ["lat", "lon", "alt"]]
+
+    state_dim = first(S.dims)
+    action_dim = length(amin)
+    V() = ContinuousNetwork(Chain(Dense(state_dim, 64, relu), Dense(64, 64, relu), Dense(64, 1)))
+
+    function PPO_A()
+        base = Chain(Dense(state_dim, 64, relu), Dense(64, 64, relu))
+        mu = ContinuousNetwork(Chain(base..., Dense(64, action_dim)))
+        logΣ = ContinuousNetwork(Chain(base..., Dense(64, action_dim)))
+        SquashedGaussianPolicy(mu, logΣ, 0.0000001)
+    end
+
+    on_policy = ActorCritic(PPO_A(), V())
+
+    off_policy = (S=S,
+                  ΔN=300,
+                  N=600, # NOTE: was 30_000 (then 100)
+                  max_steps=1000,
+                  log=(fns=[],))
+
+    return PPO(; π=on_policy, off_policy...)
 end
 
 
@@ -58,7 +121,7 @@ function reward_mod(sarsp::NamedTuple)
     return r
 end
 
-
+# pass (mdp, s) check isterminal(mdp, s)
 function extract_info(info)
     if info["done"] == true
         d = info["distance"]
@@ -76,7 +139,7 @@ function collect_and_train_gda(mdp)
     # 1. run AST with data collection ON (records distance and rate at terminal state)
     @info "Running AST to collect dataset..."
     mdp_info = InfoCollector(mdp, extract_info)
-    @time π_train = solve(sac_solver(mdp_info), mdp_info)
+    @time π_train = solve(td3_solver(mdp_info), mdp_info)
 
     # 2. compile data set of ([d,r], y)
     dataset = mdp_info.dataset
@@ -100,7 +163,7 @@ function collect_and_train_gda(mdp)
     # 5 (external). re-run solver using reward augmented MDP
     @info "Re-running AST with reward augmentation..."
     mdp_aug = RewardMod(mdp, reward_mod)
-    @time π_aug = solve(sac_solver(mdp_aug), mdp_aug)
+    @time π_aug = solve(td3_solver(mdp_aug), mdp_aug)
 
     return π_aug, dataset
 end
