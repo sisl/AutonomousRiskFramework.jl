f7e231586fada0df286def19b506ed0130d41931
Robert Moss <rmoss92@gmail.com>
2022-02-01 17:42:46 -0800

    Linux changes on cheonan for CARLA (port 3000)

---
+++ CARLAIntegration/adversarial_carla_env/carla-start.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=1 $CARLA_ROOT/CarlaUE4.sh -carla-rpc-port=3000 -windowed -ResX=320 -ResY=240 -benchmark -fps=10 -quality-level=Low
--- CARLAIntegration/adversarial_carla_env/test/run_episode.py
+++ CARLAIntegration/adversarial_carla_env/test/run_episode.py
@@ -11,15 +11,15 @@ sensors = [
         'lon': {'mean': 0, 'std': 0.0001, 'upper': 10, 'lower': -10},
         'alt': {'mean': 0, 'std': 0.00000001, 'upper': 0.0000001, 'lower': 0},
     },
-    {
-        'id': 'OBSTACLE',
-        'distance': {'mean': 0, 'std': 1, 'upper': 10, 'lower': -10},
-    },
-    {
-        'id': 'CAMERA',
-        'dynamic_noise_std': {'mean': 0, 'std': 0.001, 'upper': 1, 'lower': 0},
-        'exposure_compensation': {'mean': 0, 'std': 0.5, 'upper': 1, 'lower': -1},
-    },
+    # {
+    #     'id': 'OBSTACLE',
+    #     'distance': {'mean': 0, 'std': 1, 'upper': 10, 'lower': -10},
+    # },
+    # {
+    #     'id': 'CAMERA',
+    #     'dynamic_noise_std': {'mean': 0, 'std': 0.001, 'upper': 1, 'lower': 0},
+    #     'exposure_compensation': {'mean': 0, 'std': 0.5, 'upper': 1, 'lower': -1},
+    # },
     # {
     #     'id': 'COLLISION',
     #     'normal_impulse': {'mean': 0, 'std': 1, 'upper': 10, 'lower': -10},
@@ -41,14 +41,16 @@ seeds = [3] # 228, 92, 103 (Random), 1 (Random Scenario4), 3 (Random Scenario2)
 scenarios = ["Scenario2"] # ["scenario2", Scenario4"]:
 for seed in seeds:
     for scenario_type in scenarios:
-        env = gym.make('adv-carla-v0', sensors=sensors, seed=seed, scenario_type=scenario_type, weather=weather)
+        env = gym.make('adv-carla-v0', sensors=sensors, seed=seed, scenario_type=scenario_type, weather=weather, port=3000)
         obs = env.reset()
         t = r = 0
         done = False
         σ = 0.00001 # noise standard deviation (1.11 meters)
         while not done:
             t += 1
-            if len(sensors) == 2:
+            if len(sensors) == 1:
+                action = np.array([2*σ, 2*σ, 0]) # lat/lon/alt
+            elif len(sensors) == 2:
                 action = np.array([2*σ, 2*σ, 0, 0]) # lat/lon/alt/distance
             elif len(sensors) == 3:
                 action = np.array([2*σ, 2*σ, 0, 0, 0.05, -0.5]) # lat/lon/alt/distance/dynamic_noise_std/exposure_compensation
